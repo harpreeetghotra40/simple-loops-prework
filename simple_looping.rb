bab1376c5b99081ad21e40d9e7fe906aa244d125
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-03-21 22:26:05 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -15,8 +15,6 @@ def loop_iterator(number_of_times)
   end
 end
 
-loop_iterator
-
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
